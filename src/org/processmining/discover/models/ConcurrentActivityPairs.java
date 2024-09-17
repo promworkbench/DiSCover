07b46cc73f72158737b5c56ba342cded1b9f5565
hmwverbeek <h.m.w.verbeek@tue.nl>
2024-09-17 14:25:56 +0200

    Add additional check for concurrency pair: same predecessor and same successor.

--- src/org/processmining/discover/models/ConcurrentActivityPairs.java
+++ src/org/processmining/discover/models/ConcurrentActivityPairs.java
@@ -3,6 +3,8 @@ package org.processmining.discover.models;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.ujmp.core.Matrix;
+
 public class ConcurrentActivityPairs {
 
 	private ActivityPair pairs[];
@@ -13,7 +15,9 @@ public class ConcurrentActivityPairs {
 		for (int fromIdx = 1; fromIdx < alphabet.size(); fromIdx++) {
 			for (int toIdx = 1; toIdx < fromIdx; toIdx++) {
 				if (matrix.get(fromIdx, toIdx) > 0 && matrix.get(toIdx, fromIdx) > 0) {
-					pairs.add(new ActivityPair(fromIdx, toIdx));
+					if (haveSamePredecessor(matrix, alphabet, fromIdx, toIdx) && haveSameSuccessor(matrix, alphabet, fromIdx, toIdx)) {
+						pairs.add(new ActivityPair(fromIdx, toIdx));
+					}
 				}
 			}
 		}
@@ -31,5 +35,29 @@ public class ConcurrentActivityPairs {
 	
 	public int size() {
 		return size;
+	}
+	
+	private boolean haveSamePredecessor(ActivityMatrix matrix, ActivityAlphabet alphabet, int fromIdx, int toIdx) {
+		for (int idx = 1; idx < alphabet.size(); idx++) {
+			if (idx == fromIdx || idx == toIdx) {
+				continue;
+			}
+			if (matrix.get(idx, fromIdx) > 0 && matrix.get(idx, toIdx) > 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean haveSameSuccessor(ActivityMatrix matrix, ActivityAlphabet alphabet, int fromIdx, int toIdx) {
+		for (int idx = 1; idx < alphabet.size(); idx++) {
+			if (idx == fromIdx || idx == toIdx) {
+				continue;
+			}
+			if (matrix.get(fromIdx, idx) > 0 && matrix.get(toIdx, idx) > 0) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
