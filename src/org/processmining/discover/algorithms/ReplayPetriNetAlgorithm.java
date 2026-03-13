4774be718fd0c86ca9279dc2c536eaa654224316
hmwverbeek <h.m.w.verbeek@tue.nl>
2026-03-13 12:15:37 +0100

    Avoid the fitness from getting below 0.

--- src/org/processmining/discover/algorithms/ReplayPetriNetAlgorithm.java
+++ src/org/processmining/discover/algorithms/ReplayPetriNetAlgorithm.java
@@ -53,7 +53,7 @@ public class ReplayPetriNetAlgorithm {
 			 * Use +1 to favor traces with more consumed and produced tokens even if the
 			 * trace fits perfectly. 
 			 */
-			return 1.0 - ((0.5 * (missing+1)) / consumed) - ((0.5 * (remaining+1)) / produced);
+			return 1.0 - ((0.5 * (missing < consumed ? missing+1 : missing)) / consumed) - ((0.5 * (remaining < produced ? remaining+1 : remaining)) / produced);
 		}
 	};
 
